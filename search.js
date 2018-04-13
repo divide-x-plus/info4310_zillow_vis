6a3ae6c5895c945d56b76e9a386c8f467c01e77b
Kenta Takatsu <kt426@cornell.edu>
2018-04-13 11:28:21 -0400

    show/hide div for search bar

--- search.js
+++ search.js
@@ -117,3 +117,75 @@ function filter_by_neighborhood(data, neighborhood) {
   })
   return result;
 }
+
+function plot_hist(data){
+  var PADDING = 20;
+  // show div
+  document.getElementById('hist_price').style.display = "block";
+  d3.select('.content').remove();
+
+  var g = d3.select('.svg_price')
+  .append('g')
+  .attr('class', 'content')
+  .attr('transform', 'translate('+PADDING+','+PADDING+')');
+
+
+  g.append('rect')
+  .attr('width', 250)
+  .attr('height', 150)
+  .attr('fill', 'none')
+  .attr('stroke', 'black');
+
+  g.append('circle')
+  .attr('cx', 10)
+  .attr('cy', 10)
+  .attr('r', 10);
+
+  var prices = d3.map(data, d=>Number(d["Rent Amount"]));
+
+  console.log(prices);
+
+  g.append('text')
+  .attr('x', 2*PADDING)
+  .attr('y', 150-PADDING/2)
+  .style('text-anchor', 'middle')
+  .text('Close')
+  .attr('font-size', '12px')
+  .attr('fill', 'black')
+  .on('mouseover', function(d){
+    d3.select(this).transition().duration(300)
+    .attr('font-size', '15px')
+    .attr('fill', 'pink')
+  })
+  .on('mouseout', function(d){
+    d3.select(this).style("cursor", "pointer")
+    .transition().duration(300)
+    .attr('font-size', '12px')
+    .attr('fill', 'black')
+  })
+  .on('click', function(d){
+    document.getElementById('hist_price').style.display = "none";
+  });
+
+  g.append('text')
+  .attr('x', 250-2*PADDING)
+  .attr('y', 150-PADDING/2)
+  .style('text-anchor', 'middle')
+  .text('Enter')
+  .attr('font-size', '12px')
+  .attr('fill', 'black')
+  .on('mouseover', function(d){
+    d3.select(this).style("cursor", "pointer")
+    .transition().duration(300)
+    .attr('font-size', '15px')
+    .attr('fill', 'pink')
+  })
+  .on('mouseout', function(d){
+    d3.select(this).transition().duration(300)
+    .attr('font-size', '12px')
+    .attr('fill', 'black')
+  })
+  .on('click', function(d){
+    document.getElementById('hist_price').style.display = "none";
+  });
+}
